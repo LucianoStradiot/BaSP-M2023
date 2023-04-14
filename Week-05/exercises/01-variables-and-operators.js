02d1cd38ea0b3d1713c7e88449d1ff4c5942f715
LucianoStradiot <lstradiot15@gmail.com>
2023-04-14 12:12:47 -0300

    BaSP-W05-2023: Finish the exercises 'Variables and Operators'

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,34 @@
+//Exercise 1
+    //Exercise 1.a
+
+    console.log('Exercise 1.a');
+
+    var num1, num2;
+    num1 = 10;
+    num2 = 20;
+
+    numbersSum = num1 + num2;
+    console.log(numbersSum);
+
+    //Exercise 1.b
+
+    console.log('Exercise 1.b');
+
+    var name1, name2;
+
+    name1 = 'Luis';
+    name2 = 'Andrea';
+
+    console.log('The names are: ' + name1 + ' and ' + name2);
+
+    //Exercise 1.c
+
+    console.log('Exercise 1.c');
+
+    var name1, name2;
+
+    name1 = 'Luciano';
+    name2 = 'Josefina';
+
+    stringsSum = name1.length + name2.length
+    console.log(stringsSum);
